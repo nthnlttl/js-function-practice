f254cf128d61ead08fd1b9f51c1f0b090fe93b57
Nathan Little <nathanlittle@Nathans-MacBook-Air.local>
2021-08-31 15:18:01 -0400

    Vowel Function completed

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -16,13 +16,22 @@ function max(firstNum, secondNum){
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
-
+function maxOfThree(firstNum, secondNum, thirdNum){
+    return Math.max(firstNum, secondNum, thirdNum)
+  
+}
 
 
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
-
+function isVowel(char) {
+  if (char === "a") { 
+    return true;
+ } else {
+   return false;
+ }
+ }
 
 
   // ---------------------
